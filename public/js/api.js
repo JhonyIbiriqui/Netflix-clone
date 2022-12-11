890b3d1bde0b1dbd0cbdb7b68f0c95c68686c5b2
jhony ibiriqui <jhonyibiriqui200@hotmail.com>
2022-12-11 03:14:42 -0300

    Netflix-clone

---
+++ public/js/api.js
@@ -0,0 +1,10 @@
+let api_key = "0b12780a22f0090dd34815e0b844aef8"
+
+let img_url ="https://image.tmdb.org/t/p/w500/"
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
+
+
+
+
+
